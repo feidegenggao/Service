56e41a67a9a4b014ae9ad9c87cc798dc49b67d65
lxf <1989xiaofei2008@gmail.com>
2014-12-07 12:08:13 +0800

    format LogUtil.java

--- Service/src/com/lxf/util/LogUtil.java
+++ Service/src/com/lxf/util/LogUtil.java
@@ -4,10 +4,11 @@ import android.util.Log;
 
 public final class LogUtil {
 	private static final String TAG = "LogUtil";
-	public static void d(String msg){
+
+	public static void d(String msg) {
 		d(TAG, msg);
 	}
-	
+
 	public static void d(String TAG, String msg) {
 		Log.d(TAG, msg);
 	}
