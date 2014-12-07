ec2edb1804b1ae16d9685fdd8a30ffd604a4abf6
飞得更高 <1989xiaofei2008@gmail.com>
2014-12-07 12:07:32 +0800

    Update LogUtil.java

--- Service/src/com/lxf/util/LogUtil.java
+++ Service/src/com/lxf/util/LogUtil.java
@@ -3,6 +3,11 @@ package com.lxf.util;
 import android.util.Log;
 
 public final class LogUtil {
+	private static final String TAG = "LogUtil";
+	public static void d(String msg){
+		d(TAG, msg);
+	}
+	
 	public static void d(String TAG, String msg) {
 		Log.d(TAG, msg);
 	}
